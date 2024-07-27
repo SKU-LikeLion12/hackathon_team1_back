e3f532cfee46e4d62bdfcea3f155b2f6cb0258eb
KII1ua <sung25480@naver.com>
2024-07-27 16:57:00 +0900

    better than

--- src/main/java/spring/likelionpractice/controller/MemberController.java
+++ src/main/java/spring/likelionpractice/controller/MemberController.java
@@ -1,6 +1,8 @@
 package spring.likelionpractice.controller;
 
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -9,6 +11,8 @@ import spring.likelionpractice.domain.Member;
 import spring.likelionpractice.service.MemberService;
 
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,15 +41,65 @@ public class MemberController {
 
     // 수정 필요
     @PutMapping("/member")
-    public MemberResponse changeMemberInfo(@RequestParam MemberUpdateRequest request) throws IOException {
+    public MemberResponse changeMemberInfo(@RequestParam MemberUpdateRequest request, @RequestParam(required = false) MultipartFile image) throws IOException {
         Member findMember = memberService.changeInfo(request.getToken(), request.getName(), request.getPhone(), request.getNoSmk(),
                                                     request.getStartSmk(), request.getAmountSmk(), request.getPrice(), request.getCiga(),
-                                                    request.getTar());
+                                                    request.getTar(), image);
         return new MemberResponse(findMember.getUserId(), findMember.getName());
     }
 
     @DeleteMapping("/member")
     public void deleteMember(@RequestBody MemberDeleteRequest request) {
         memberService.deleteMember(request.getToken());
+    }
+
+    @GetMapping("/member/info")
+    public Map<String, Object> getMainInfo(@RequestHeader("Authorization") String bearerToken) {      // 로그인 했을때 메인 페이지 출력
+        String token = bearerToken.replace("Bearer ", "");
+        Member member = memberService.tokentoMember(token);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("금연한지 얼마나 지났는지", memberService.stateOfChange(member));
+        map.put("피우지 않은 담배 개수", memberService.calcNotSmoked(member));
+        map.put("절약한 금액", memberService.calcSaveMoney(member));
+        map.put("늘어난 수명", memberService.calcLife(member));
+        map.put("총 흡연기간", memberService.sumSmokedDate(member));
+        map.put("소비한 금액", memberService.sumMoney(member));
+        map.put("삼킨 타르양", memberService.sumTar(member));
+
+        return map;
+    }
+
+    @GetMapping("/main/info/change-state")      // 금연한지 몇시간 경과하였는지
+    public Map<String, Object> getChangeState(@RequestHeader("Authorization") String bearerToken) {
+        String token = bearerToken.replace("Bearer ", "");
+        Member member = memberService.tokentoMember(token);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("금연한지 경과한 시간", memberService.stateOfChange(member));
+
+        return map;
+    }
+
+    @GetMapping("/main/info/detail-save")      // 절약한 금액 자세히 보기
+    public Map<String, Object> getMainInfoDetailSave(@RequestHeader("Authorization") String bearerToken) {
+        String token = bearerToken.replace("Bearer ", "");
+        Member member = memberService.tokentoMember(token);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("절약한 금액", memberService.calcSaveMoney(member));
+
+        return map;
+    }
+
+    @GetMapping("/main/info/detail-life")      // 늘어난 수명 자세히 보기
+    public Map<String, Object> getMainInfoDetailLife(@RequestHeader("Authorization") String bearerToken) {
+        String token = bearerToken.replace("Bearer ", "");
+        Member member = memberService.tokentoMember(token);
+
+        Map<String, Object> map = new LinkedHashMap<>();
+        map.put("늘어난 수명", memberService.calcLife(member));
+
+        return map;
     }
 }
--- src/main/java/spring/likelionpractice/service/MemberService.java
+++ src/main/java/spring/likelionpractice/service/MemberService.java
@@ -3,9 +3,11 @@ package spring.likelionpractice.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 import spring.likelionpractice.domain.Member;
 import spring.likelionpractice.repository.MemberRepository;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -25,9 +27,18 @@ public class MemberService {
 
     @Transactional
     public Member changeInfo(String token, String name, String phone, LocalDate noSmk, LocalDate startSmk,
-                             int amountSmk, int price, int ciga, int tar) {
+                             int amountSmk, int price, int ciga, int tar, MultipartFile image) throws IOException {
         Member member = tokentoMember(token);
         if (member == null) return null;
+
+        byte[] images = null;
+        if (image != null) {
+            // 이미지를 수정했을때
+            images = image.getBytes();      // IOException 예외 처리를 해줘야함
+        } else {
+            // 이미지를 수정하지 않았을때
+            images = member.getImage();
+        }
         member.setName(name);
         member.setPhone(phone);
         member.setNoSmk(noSmk);
@@ -36,6 +47,7 @@ public class MemberService {
         member.setPrice(price);
         member.setCiga(ciga);
         member.setTar(tar);
+        member.setImage(images);
         return member;
     }
 
@@ -81,29 +93,29 @@ public class MemberService {
 
     // 내 상태변화
     @Transactional
-    public int stateOfChange(String token) {            // 금연한지 얼마나 지났는지
-        Member member = tokentoMember(token);
+    public int stateOfChange(Member memberId) {            // 금연한지 얼마나 지났는지
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return (int) ChronoUnit.DAYS.between(noSmkDay, localDate) * 1440;
     }
 
     @Transactional
-    public int calcNotSmoked(String token) {            // 피우지 않은 담배 개수 = (현재 날짜 - 금연시작 일시) * 하루 흡연량
-        Member member = tokentoMember(token);
+    public int calcNotSmoked(Member memberId) {            // 피우지 않은 담배 개수 = (현재 날짜 - 금연시작 일시) * 하루 흡연량
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return (int) (ChronoUnit.DAYS.between(noSmkDay, localDate) * member.getAmountSmk());
     }
 
     @Transactional
-    public int calcSaveMoney(String token) {            // 절약한 금액 = Math.round((현재 날짜 - 금연시작 일시 * 하루 흡연량 / 한갑당 개비 수)) * 담배 가격
-        Member member = tokentoMember(token);
+    public int calcSaveMoney(Member memberId) {            // 절약한 금액 = Math.round((현재 날짜 - 금연시작 일시 * 하루 흡연량 / 한갑당 개비 수)) * 담배 가격
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return Math.round((ChronoUnit.DAYS.between(noSmkDay, localDate) * member.getAmountSmk())) * member.getPrice();
     }
 
     @Transactional
-    public int calcLife(String token) {             // 늘어난 수명 = (현재 날짜 - 금연시작 일시) * 하루 흡연량 * 11
-        Member member = tokentoMember(token);
+    public int calcLife(Member memberId) {             // 늘어난 수명 = (현재 날짜 - 금연시작 일시) * 하루 흡연량 * 11
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return (int) (ChronoUnit.DAYS.between(noSmkDay, localDate) * 1440 * 11);
     }
@@ -111,22 +123,22 @@ public class MemberService {
 
     // 과거 흡연 정보
     @Transactional
-    public int sumSmokedDate(String token) {        // 총 흡연기간 = (금연시작 일시 - 흡연 시작 일시)
-        Member member = tokentoMember(token);
+    public int sumSmokedDate(Member memberId) {        // 총 흡연기간 = (금연시작 일시 - 흡연 시작 일시)
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return (int) ChronoUnit.DAYS.between(member.getStartSmk(), noSmkDay);
     }
 
     @Transactional
-    public int sumMoney(String token) {              // 소비한 금액 = (금연 시작 일시 - 흡연시작 일시) * 담배가격
-        Member member = tokentoMember(token);
+    public int sumMoney(Member memberId) {              // 소비한 금액 = (금연 시작 일시 - 흡연시작 일시) * 담배가격
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return (int) ChronoUnit.DAYS.between(member.getStartSmk(), noSmkDay) * member.getPrice();
     }
 
     @Transactional
-    public int sumTar(String token) {                // 삼킨 타르양 = (금연 시작 일시 - 흡연 시작 일시) * 하루 흡연량 * 타르양
-        Member member = tokentoMember(token);
+    public int sumTar(Member memberId) {                // 삼킨 타르양 = (금연 시작 일시 - 흡연 시작 일시) * 하루 흡연량 * 타르양
+        Member member = findById(memberId.getId());
         LocalDate noSmkDay = member.getNoSmk();
         return (int) ChronoUnit.DAYS.between(member.getStartSmk(), noSmkDay) * member.getAmountSmk() * member.getTar();
     }
